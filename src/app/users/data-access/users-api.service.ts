9b688cf6fc17e9dd6684cd26e329b0a6cf689211
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-15 17:08:02 +0600

    refactor: add environment variable for request

--- src/app/users/data-access/users-api.service.ts
+++ src/app/users/data-access/users-api.service.ts
@@ -1,14 +1,16 @@
 import { Observable } from 'rxjs'
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+
 import { UserInterface } from './types/user.interface'
+import { environment } from '../../../environments/environment.development'
 
 @Injectable()
 export class UsersApiService {
 	constructor(private http: HttpClient) {}
 
 	getUsers(): Observable<UserInterface[]> {
-		const url = 'https://jsonplaceholder.typicode.com/users'
+		const url = `${environment.apiUrl}/users`
 		return this.http.get<UserInterface[]>(url)
 	}
 }
