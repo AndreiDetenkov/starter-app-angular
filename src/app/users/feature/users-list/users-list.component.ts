c4d36f08458f6246a0a67e03d226a09687d011e6
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-16 21:29:23 +0600

    feat: add take operator for observable

--- src/app/users/feature/users-list/users-list.component.ts
+++ src/app/users/feature/users-list/users-list.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { take } from 'rxjs'
 
 import { UsersService } from '../../data-access/users.service'
 import { GetUsersUseCase } from '../../data-access/get-users.usecase'
 import { GetUsersService } from '../../data-access/get-users.service'
 import { UserInterface } from '../../data-access/types/user.interface'
 import { UserCardComponent } from '../../ui/user-card/user-card.component'
-
 @Component({
 	selector: 'app-users-list',
 	standalone: true,
@@ -33,6 +33,7 @@ export class UsersListComponent implements OnInit {
 	ngOnInit(): void {
 		this.getUsersUseCase
 			.execute()
+			.pipe(take(1))
 			.subscribe((users: UserInterface[]) => (this.usersService.setUsers = users))
 	}
 }
