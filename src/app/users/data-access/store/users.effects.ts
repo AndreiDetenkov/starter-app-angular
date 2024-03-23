3a3c345289f72bc5b5cbba9bc2600590a8f19bb2
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-03-23 23:14:58 +0600

    feat: save users in storage after creating

--- src/app/users/data-access/store/users.effects.ts
+++ src/app/users/data-access/store/users.effects.ts
@@ -1,4 +1,4 @@
-import { catchError, exhaustMap, map, of, tap } from 'rxjs'
+import { catchError, exhaustMap, map, of, switchMap, tap } from 'rxjs'
 import { inject } from '@angular/core'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
 
@@ -6,6 +6,9 @@ import { UsersClientService } from '../services/users-client.service'
 import { usersActions } from './users.actions'
 import { UserInterface } from '../types/user.interface'
 import { NotifyService } from '../../../shared/services/notify.service'
+import { StorageService } from '../../../shared/services/storage.service'
+import { Store } from '@ngrx/store'
+import { usersFeature } from './users.reducer'
 
 export const getUsersEffect = createEffect(
   (actions$ = inject(Actions), usersClientService = inject(UsersClientService)) => {
@@ -25,9 +28,19 @@ export const getUsersEffect = createEffect(
 )
 
 export const createUserEffect = createEffect(
-  (actions$ = inject(Actions), notifyService = inject(NotifyService)) => {
+  (
+    actions$ = inject(Actions),
+    notifyService = inject(NotifyService),
+    storageService = inject(StorageService),
+    store = inject(Store),
+  ) => {
     return actions$.pipe(
       ofType(usersActions.createUser),
+      switchMap(() =>
+        store
+          .select(usersFeature.selectUsers)
+          .pipe(map((users) => storageService.set('users', users))),
+      ),
       tap(() => notifyService.success('User created!')),
     )
   },
